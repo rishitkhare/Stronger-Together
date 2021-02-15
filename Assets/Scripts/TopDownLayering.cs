b72336deddfbe21286b79efe6f412db9ffb24999
Rishit Khare <kharerishit@gmail.com>
2021-02-15 09:09:14 -0800

    Art Implementation
    Added Computer and Keyslot sprites
    
    mapped sprites to gameobjects
    
    added tileset to scene

---
+++ Assets/Scripts/TopDownLayering.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownLayering : MonoBehaviour {
+    public int orderInLayer;
+    SpriteRenderer sp;
+
+    // Start is called before the first frame update
+    void Start() {
+        sp = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        sp.sortingOrder = -4 * (orderInLayer + (int) transform.position.y);
+    }
+}
