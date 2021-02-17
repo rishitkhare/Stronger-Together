e706ad460631264932f19b88e54b52767537a91d
Rishit Khare <kharerishit@gmail.com>
2021-02-16 20:37:41 -0800

    Camera Animator from script

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -9,9 +9,19 @@ public class CameraHandler : MonoBehaviour
     public float rotationSpeed;
     public float center;
     private float currentAngle;
+
+    private Transform childTransform;
+
+    private readonly int currentAngleAnimatorHash = Animator.StringToHash("Angle");
+    private readonly int isControllingAnimatorHash = Animator.StringToHash("IsControlling");
+
+    private Animator anim;
+
     void Start()
     {
-        transform.rotation = Quaternion.AngleAxis(center, Vector3.forward);
+        anim = gameObject.GetComponent<Animator>();
+        childTransform = transform.GetChild(0);
+        childTransform.rotation = Quaternion.AngleAxis(center, Vector3.forward);
         currentAngle = 0;
     }
 
@@ -37,7 +47,11 @@ public class CameraHandler : MonoBehaviour
                     else { currentAngle -= rotationSpeed * Time.deltaTime; }
                 }
             }
-            transform.rotation = Quaternion.AngleAxis(center + currentAngle, Vector3.forward);
         }
+
+        childTransform.rotation = Quaternion.AngleAxis(center + currentAngle, Vector3.forward);
+
+        anim.SetFloat(currentAngleAnimatorHash, -(currentAngle - maxDeviationFromCenter) / (maxDeviationFromCenter / 5f));
+        anim.SetBool(isControllingAnimatorHash, playerIsControlling);
     }
 }
