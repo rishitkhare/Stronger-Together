80d7b0bed5b063b3506f2484bb8cf2525503bd81
Rishit Khare <kharerishit@gmail.com>
2021-02-19 13:46:43 -0800

    Repurpose scene transition to restart current level
    Here you go, Alex!

--- Assets/Scripts/Elevator Scripting/SceneTransition.cs
+++ Assets/Scripts/Elevator Scripting/SceneTransition.cs
@@ -5,6 +5,10 @@ public class SceneTransition : MonoBehaviour {
     public ElevatorScript greenElevator;
     public ElevatorScript purpleElevator;
 
+    private bool resetLevel;
+
+    private bool nextLevel;
+
     private Animator anim;
 
     private readonly int FadeToBlackHash = Animator.StringToHash("Fade to Black");
@@ -17,14 +21,39 @@ public class SceneTransition : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (greenElevator.IsOpen && purpleElevator.IsOpen) {
+            nextLevel = true;
+        }
+
+
+        if(nextLevel) {
             LevelEnd();
         }
+        else if(resetLevel) {
+            FadeToBlackAndReload();
+        }
+    }
+
+    public void RestartLevel() {
+        resetLevel = true;
+    }
+
+    public void GoToNextLevel() {
+        nextLevel = true;
     }
 
     private void LevelEnd() {
         if(anim.GetCurrentAnimatorStateInfo(0).IsName("TransitionScene")) {
+            //load next scene
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
         anim.SetTrigger(FadeToBlackHash);
+    }
+
+    private void FadeToBlackAndReload() {
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("TransitionScene")) {
+            //reload current scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        anim.SetTrigger(FadeToBlackHash)
     }
 }
