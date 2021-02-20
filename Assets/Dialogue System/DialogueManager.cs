b4aa6795160e64542253586bd073b70e94164420
Rishit Khare <kharerishit@gmail.com>
2021-02-20 04:39:47 -0800

    Dialog system UI
    all that's left is to parse the txt and time the dialog.

--- Assets/Dialogue System/DialogueManager.cs
+++ Assets/Dialogue System/DialogueManager.cs
@@ -8,7 +8,6 @@ public class DialogueManager : MonoBehaviour {
     // Animator Hashes - these allow Unity to save processing power
     // by converting the string to integer Animator Parameters
     private readonly int IsOpenHash = Animator.StringToHash("DialogueIsOpen");
-    private readonly int IsSpeakingHash = Animator.StringToHash("DialogueIsSpeaking");
 
     // singleton instance
     public static DialogueManager Instance;
@@ -21,46 +20,43 @@ public class DialogueManager : MonoBehaviour {
     public TMP_Text dialogueText;
     public Image portraitImage;
 
+    // settings for dialogue
+    public float dialogueSpeedCPS = 15f; //Speed in chars per second
+
+    // timer to slow dialogue scrollrate
+    private float dialogueSpeedTimer = 0f;
+
     // "IsOpen" is parameter controlling whether or not 
     // the dialogue box is open
     // "Speaking" parameter indicates whether still typing or not
     // --> if not typing, animate the arrow thing on the box
-    public Animator animator;
+    private Animator animator;
 
     private Queue<Dialogue> dialogueQueue;
 
     private void Start() {
         Instance = this;
         dialogueQueue = new Queue<Dialogue>();
-
+        animator = gameObject.GetComponent<Animator>();
     }
     //This function will be called from DialogueTrigger. It will initiate a series
     // of coroutines (methods that run across multiple frames) that each type out the text for dialogue.
 
     public void StartDialogue(Dialogue[] dialogue) {
 
+        animator.SetBool(IsOpenHash, true);
+
+        dialogueQueue.Clear();
+
         foreach (Dialogue dialog in dialogue) {
-
-            animator.SetBool(IsOpenHash, true);
-            animator.SetBool(IsSpeakingHash, true);
-
-            //Set the name and 
-            nameText.text = dialog.name;
-            portraitImage.sprite = dialog.portrait;
-
-            dialogueQueue.Clear();
-
-
+            dialog.Parse();
             dialogueQueue.Enqueue(dialog);
-
-            DisplayNextSentence();
         }
+
+        DisplayNextSentence();
     }
 
     public void DisplayNextSentence() {
-
-        // starting new sentence, arrow clicker begone
-        animator.SetBool(IsSpeakingHash, true);
 
         if (dialogueQueue.Count == 0) {
 
@@ -69,7 +65,14 @@ public class DialogueManager : MonoBehaviour {
 
         }
 
-        string typedText = dialogueQueue.Dequeue().dialogText;
+        Dialogue dialog = dialogueQueue.Dequeue();
+
+        //Set the name and portrait
+        nameText.text = dialog.name;
+        portraitImage.sprite = dialog.portrait;
+
+        //type out the text using a coroutine (and skip the previous dialogue)
+        string typedText = dialog.dialogText;
         StopAllCoroutines();
         StartCoroutine(TypeSentence(typedText));
 
@@ -79,27 +82,20 @@ public class DialogueManager : MonoBehaviour {
 
         dialogueText.text = "";
         foreach (char letter in sentence.ToCharArray()) {
-
             dialogueText.text += letter;
+            dialogueSpeedTimer = 0f;
 
-            yield return null;
+            while(dialogueSpeedTimer < 1f / dialogueSpeedCPS) {
+                dialogueSpeedTimer += Time.deltaTime;
+                yield return null;
+            }
 
         }
-
-
-        EndSpeaking();
-
-    }
-
-    void EndSpeaking() {
-
-        animator.SetBool(IsSpeakingHash, false);
 
     }
 
     void CloseDialogueBox () {
 
-        EndSpeaking();
         animator.SetBool(IsOpenHash, false);
 
     }
